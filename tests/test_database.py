ed6b2734ccbfe5a4d517403966c884fe6b11be43
Calel <j.vargas.9@estudiantec.cr>
2025-11-16 23:15:46 -0600

    Implementación de pruebas unitarias y CI

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from database import GrupoCajetaDB
 
 @pytest.fixture
